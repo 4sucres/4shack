3e49adcb44081a6254215fb4a9ab42359c8a6341
Simon Rubuano <sr@mgk.dev>
2020-07-29 17:09:20 +0200

    Add a disk migration command

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,31 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('migrate:from-s3', function () {
+    $count = \App\Models\Image::count();
+    $bar = $this->output->createProgressBar($count);
+
+    \App\Models\Image::query()
+        ->orderBy('created_at')
+        ->chunk(1000, function ($chunk) use ($bar) {
+            $s3 = \Storage::disk('s3');
+            $local = \Storage::disk('public');
+
+            foreach ($chunk as $file) {
+                try {
+                    $local->put(
+                        $file->path,
+                        $s3->get($file->path)
+                    );
+                } catch (\Throwable $th) {
+                    report($th);
+                    $this->error($th->getMessage());
+                }
+                $bar->advance();
+            }
+        });
+
+    $bar->finish();
+    $this->line('');
+});
